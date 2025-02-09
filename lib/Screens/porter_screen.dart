cb37de986b729f916b32c2bf73024592d263793a
aacosta123 <alex264sg@comcast.net>
2025-02-08 16:28:25 -0800

    update

--- lib/Screens/porter_screen.dart
+++ lib/Screens/porter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:housing_portal_plus/Screens/home_screen.dart';
 
 class PorterScreen extends StatelessWidget {
   const PorterScreen({super.key});
@@ -8,9 +9,38 @@ class PorterScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Porter College'),
+        automaticallyImplyLeading: false,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home, color: Colors.white),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                  (route) => false,
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.white),
+              onPressed: () {
+                Scrollable.ensureVisible(
+                  context,
+                  duration: Duration(milliseconds: 500),
+                );
+              },
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Image(
+          width: 750 ,
           image: AssetImage('assets/download.jpg')
           ), 
       ),
