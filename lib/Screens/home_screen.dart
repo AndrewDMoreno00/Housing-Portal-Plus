03dc282019befb68a10bef8ddc453e174efc3f89
aalexSC <alex264SG@comcast.net>
2025-03-30 17:17:08 -0700

    created function to create a box for each college

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:housing_portal_plus/Screens/porter_screen.dart';
+import 'package:housing_portal_plus/Screens/oakes_screen.dart';
+import 'package:housing_portal_plus/Screens/crown_screen.dart'; 
+import 'package:housing_portal_plus/Screens/google_map.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable
     double screenWidth = MediaQuery.of(context).size.width;
-    
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -40,77 +44,133 @@ class HomeScreen extends StatelessWidget {
                 );
               },
             ),
+            IconButton(
+              icon: Icon(Icons.map, color: Colors.white),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapScreen()),
+                  (route) => false,
+                );
+              },
+            ),
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SearchAnchor(
-                builder: (BuildContext context, SearchController controller) {
-                  return SearchBar(
-                    controller: controller,
-                    padding: const WidgetStatePropertyAll<EdgeInsets>(
-                      EdgeInsets.symmetric(horizontal: 16.0),
-                    ),
-                    onTap: () {
-                      controller.openView();
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SearchAnchor(
+                    builder: (BuildContext context, SearchController controller) {
+                      return SearchBar(
+                        controller: controller,
+                        padding: const WidgetStatePropertyAll<EdgeInsets>(
+                          EdgeInsets.symmetric(horizontal: 16.0),
+                        ),
+                        onTap: () {
+                          controller.openView();
+                        },
+                        onChanged: (_) {
+                          controller.openView();
+                        },
+                        leading: const Icon(Icons.search),
+                      );
                     },
-                    onChanged: (_) {
-                      controller.openView();
+                    suggestionsBuilder:
+                        (BuildContext context, SearchController controller) {
+                      return List<ListTile>.generate(5, (int index) {
+                        final String item = 'item $index';
+                        return ListTile(
+                          title: Text(item),
+                          onTap: () {
+                            controller.closeView(item);
+                          },
+                        );
+                      });
                     },
-                    leading: const Icon(Icons.search),
-                  );
-                },
-                suggestionsBuilder:
-                    (BuildContext context, SearchController controller) {
-                  return List<ListTile>.generate(5, (int index) {
-                    final String item = 'item $index';
-                    return ListTile(
-                      title: Text(item),
-                      onTap: () {
-                        controller.closeView(item);
-                      },
-                    );
-                  });
-                },
-              ),
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PorterScreen()),
-                );
-              },
-              child: Container(
-                width: screenWidth * 0.9, // 90% of screen width
-                height: screenWidth * 0.13, // Adjust this to your desired height
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
+                  ),
+                ),
+                SizedBox(height: 16),
+                CustomTile(
+                  title: 'Porter College',
                   color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(8),
+                  destinationPage: PorterScreen(),
+                  imageAsset: 'assets/squiggle.jpg',
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Porter College',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Image.asset(
-                      'assets/squiggle.jpg',
-                      width: screenWidth * 0.3, // 30% of screen width
-                      height: screenWidth * 0.2, // Maintain aspect ratio
-                      fit: BoxFit.cover,
-                    ),
-                  ],
+                SizedBox(height: 16),
+                CustomTile(
+                  title: 'Oakes College',
+                  color: Colors.orange, 
+                  destinationPage: OakesScreen(), 
+                  imageAsset: 'assets/', 
                 ),
-              ),
+                SizedBox(height: 16),
+                CustomTile(
+                  title: 'Crown College',
+                  color: Colors.blue, 
+                  destinationPage: CrownScreen(), 
+                  imageAsset: 'assets/', 
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomTile extends StatelessWidget {
+  final String title;
+  final Color color;
+  final Widget destinationPage;
+  final String imageAsset;
+
+  const CustomTile({
+    super.key,
+    required this.title,
+    required this.color,
+    required this.destinationPage,
+    required this.imageAsset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => destinationPage),
+        );
+      },
+      child: Container(
+        width: screenWidth * 0.9, // 90% of screen width
+        height: screenWidth * 0.13, // Adjust this to your desired height
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Image.asset(
+              imageAsset,
+              width: screenWidth * 0.3, // 30% of screen width
+              height: screenWidth * 0.2, // Maintain aspect ratio
+              fit: BoxFit.cover,
             ),
           ],
         ),
