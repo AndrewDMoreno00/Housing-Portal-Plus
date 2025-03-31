9d06dfe2fb761a5a252aa3c0a5c0db8ce6b599bb
aalexSC <alex264SG@comcast.net>
2025-03-30 17:33:07 -0700

    crown and oakes screen

---
+++ lib/Screens/crown_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:housing_portal_plus/Screens/home_screen.dart';
+
+class CrownScreen extends StatelessWidget {
+  const CrownScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: 
+        Text(
+          'Crown College',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home, color: Colors.white),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                  (route) => false,
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.white),
+              onPressed: () {
+                Scrollable.ensureVisible(
+                  context,
+                  duration: Duration(milliseconds: 500),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+              margin: EdgeInsets.only(top: 20),
+              width: MediaQuery.of(context).size.width * 0.9, // 90% of screen width
+                child: Image.asset(
+                  'assets/crown.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center (
+              child: Text(
+                '',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    minimumSize: Size(120, 50),
+                  ),
+                  child: Text('Button 1', style: TextStyle(color: Colors.white)),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    minimumSize: Size(120, 50),
+                  ),
+                  child: Text('Button 2', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                minimumSize: Size(260, 50),
+              ),
+              child: Text('Button 5', style: TextStyle(color: Colors.white)),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    minimumSize: Size(120, 50),
+                  ),
+                  child: Text('Button 3', style: TextStyle(color: Colors.white)),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    minimumSize: Size(120, 50),
+                  ),
+                  child: Text('Button 4', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
